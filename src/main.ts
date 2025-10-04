a8221ab6fd81607c40d860dc7445d642b0c640ed
AndreiSva <andreisva2023@gmail.com>
2025-10-04 14:27:28 -0700

    add some API

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,27 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as THREE from 'three';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const appDiv = document.querySelector<HTMLDivElement>("#app")!;
+export const mainScene = new THREE.Scene();
+let currentScene = mainScene;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function render(renderer: THREE.WebGLRenderer, camera: THREE.Camera) {
+  renderer.render(currentScene, camera);
+}
+
+export function setCurrentScene(scene: THREE.Scene) {
+  currentScene = scene;
+}
+
+export function graphicsInit() {
+  console.log("Initializing Graphics...")
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setAnimationLoop(() => {render(renderer, camera)});
+  appDiv.appendChild(renderer.domElement);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  graphicsInit();
+})
