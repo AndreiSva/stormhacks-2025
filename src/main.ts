d3686a9f62b4b634a011f3873f7a76ea4ec3ab05
AndreiSva <andreisva2023@gmail.com>
2025-10-04 14:44:49 -0700

    add triangle rendering

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,41 @@ const appDiv = document.querySelector<HTMLDivElement>("#app")!;
 export const mainScene = new THREE.Scene();
 let currentScene = mainScene;
 
+export function setCurrentScene(scene: THREE.Scene) {
+  currentScene = scene;
+}
+
 function render(renderer: THREE.WebGLRenderer, camera: THREE.Camera) {
   renderer.render(currentScene, camera);
 }
 
-export function setCurrentScene(scene: THREE.Scene) {
-  currentScene = scene;
+function addTriangles(scene: THREE.Scene, triangles: Array<Array<THREE.Vector3>>) {
+  triangles.forEach((triangle) => {
+    const a = triangle[0];
+    const b = triangle[1];
+    const c = triangle[2];
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute(
+      'position',
+      new THREE.Float32BufferAttribute([
+        a.x, a.y, a.z,
+        b.x, b.y, b.z,
+        c.x, c.y, c.z
+      ], 3)
+    );
+
+    geometry.computeVertexNormals();
+
+    const material = new THREE.MeshStandardMaterial({
+      color: 0xff5533,
+      side: THREE.DoubleSide,   // see front & back; remove if you want back-face culling
+      flatShading: true
+    });
+
+    const triangleMesh = new THREE.Mesh(geometry, material);
+    scene.add(triangleMesh);
+  });
 }
 
 export function graphicsInit() {
@@ -23,5 +52,6 @@ export function graphicsInit() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  console.log("Content Loaded");
   graphicsInit();
 })
